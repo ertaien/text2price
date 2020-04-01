b0857c8c56a2e2314f3fa26ea5fe9db7639172c5
ertai <ertai.master@gmail.com>
2020-04-01 11:28:00 +0500

    Updated regex

--- app.py
+++ app.py
@@ -3,17 +3,14 @@ import unittest
 
 
 def parser_amount(text):
-    clean_text = re.sub(r'[^\w]', ' ', text)
+    clean_text = re.sub(r'[^\w]', ' ', text.lower())
     text = re.sub('[\s+]', '', clean_text)
-    res = re.search("([0-9]+тг|[0-9]+теңге|[0-9]+тенге)", text)
-
-    if res is not None:
-        res = re.findall("[0-9]+", res.group())
-    else:
-        res = re.search("[0-9]+", text)
+    res = re.search("[0-9]+(тг|тенге|теңге|тнг|tg)", text)
 
     if res is None:
         return None
+
+    res = re.findall("[0-9]+", res.group())
 
     return int(res[0])
 
@@ -38,17 +35,19 @@ class TextAmountParser(unittest.TestCase):
         self.assertEqual(parser_amount("Cатамын, 1500 тенге"), 1500)
         self.assertEqual(parser_amount("Cатамын, 3500 тенге"), 3500)
 
+    def test_amount_lower_case(self):
+        self.assertEqual(parser_amount("Cатамын, 500 ТЕНГЕ"), 500)
+        self.assertEqual(parser_amount("Cатамын, 1500 ТГ"), 1500)
+
     def test_amount_symbols_number(self):
         self.assertEqual(parser_amount("Комбинезон және шапка, куртка - 10,000 тг."), 10000)
-        self.assertEqual(parser_amount("Комбинезон және 234шапка, куртка - 10.00.0 тг."), 10000)
-        self.assertEqual(parser_amount("Комбинезон және 342шапка, куртка - 10.00.0 тг."), 10000)
+        self.assertEqual(parser_amount("Комбинезон және 234 шапка, куртка - 10.00.0 тг."), 10000)
+        self.assertEqual(parser_amount("Комбинезон және 342 шапка, куртка - 10.00.0 тг."), 10000)
         self.assertEqual(parser_amount("Комбинезон және 345 шапка, куртка - 100.0.0 тг."), 10000)
-
-    def test_amount_without_tg(self):
-        self.assertEqual(parser_amount("Жылқы сатылады. 300 000 ға берем."), 300000)
 
     def test_amount_none(self):
         self.assertIsNone(parser_amount("Жылқы сатылады. ға берем."))
+        self.assertIsNone(parser_amount("Жылқы сатылады. 300 000 ға берем."))
 
 
 if __name__ == "__main__":
